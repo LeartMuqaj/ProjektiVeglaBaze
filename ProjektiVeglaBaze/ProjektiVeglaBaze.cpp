2a3ed6798613b272a6fa3970b93d280bb2c65cfd
Leart Muçaj <leart.muqaj321@gmail.com>
2026-01-03 13:41:42 +0100

    Versioni i pare: analizues i numrit

---
+++ ProjektiVeglaBaze/ProjektiVeglaBaze.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    // Check positive, negative, or zero
+    if (number > 0) {
+        cout << "The number is positive." << endl;
+    }
+    else if (number < 0) {
+        cout << "The number is negative." << endl;
+    }
+    else {
+        cout << "The number is zero." << endl;
+    }
+
+    // Check even or odd
+    if (number != 0) {
+        if (number % 2 == 0) {
+            cout << "The number is even." << endl;
+        }
+        else {
+            cout << "The number is odd." << endl;
+        }
+    }
+
+    return 0;
+}
+
